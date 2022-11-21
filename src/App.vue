b9fb20474ea99ed7302cd4c79274acb890c01fd6
paoloevan <paoloev1900@gmail.com>
2022-11-21 13:10:23 +0100

    add header

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,25 @@
 </script>
 
 <template>
+  <header>
+    <div class="container">
+      <div class="logo">
+        <img src="./assets/img/dark-pet-logo.png" alt="">
+      </div>
+
+      <input type="search">
+
+      <div class="text">
+        <strong>Questions? </strong>Call us: 1.800.123.4567
+      </div>
+
+      <div class="personal">
+
+      </div>
+
+    </div>
+  </header>
+  <!-- / header -->
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.scss'
 import App from './App.vue'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from '../node_modules/@fortawesome/vue-fontawesome'
+// library.add(faUserSecret)
 
-createApp(App).mount('#app')
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
