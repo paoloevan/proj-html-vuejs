75dfac5cf9a15dfe2c7751f765962bc29abbd35e
paoloevan <paoloev1900@gmail.com>
2022-11-21 16:53:36 +0100

    add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script >
 import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
 
 export default {
   name: 'App',
   components: {
     AppHeader,
-    AppMain
+    AppMain,
+    AppFooter
   }
 
 }
@@ -20,6 +22,9 @@ export default {
 
   <AppMain />
   <!-- / main -->
+
+  <AppFooter />
+  <!-- / footer -->
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,101 @@
+<script>
+export default {
+    name: 'AppFooter'
+}
+</script>
+
+<template>
+    <footer class="py-5">
+        .<div class="container">
+            <div class="row row-cols-5 justify-content-between">
+                <div class="col">
+
+                    <div class="logo mb-5">
+                        <img src="../assets/img/mobile-light-pet-logo-2x-200x52.png" alt="">
+                    </div>
+
+                    <address class="text-white text">
+                        1234 Avada Avenue Avadaville <br>
+                        info@your-company.com <br>
+                        Call us: (1) 234 5678
+                    </address>
+                </div>
+
+                <div class="col">
+                    <h4>Shop by brand</h4>
+                    <a href="#">Bed</a>
+                    <a href="#">Food</a>
+                    <a href="#">Toys</a>
+                    <a href="#">Transport</a>
+                </div>
+
+                <div class="col">
+                    <h4>Useful links</h4>
+                    <a href="#">My account</a>
+                    <a href="#">Orders</a>
+                    <a href="#">Checkout</a>
+                    <a href="#">Cart</a>
+                </div>
+
+                <div class="col call_to_action">
+                    <h4>Subscribe to our newsletter</h4>
+                    <input type="email" placeholder="Insert your email">
+                    <button>Subscribe</button>
+                </div>
+
+
+            </div>
+            <hr>
+        </div>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/sass/partials/palette' as *;
+
+footer {
+    background-color: $median--primary;
+
+    hr {
+        background-color: $median--light-primary;
+        border: none;
+        height: 1px;
+        margin: 3rem 0;
+    }
+
+    address {
+        line-height: 2rem;
+    }
+
+    .call_to_action {
+        padding: 2rem 1rem 1rem 1rem;
+        border-radius: 5px;
+        background-color: $median--light-primary;
+
+        input {
+            border: none;
+            margin-bottom: 1rem;
+            border-radius: 20px;
+            width: 100%;
+            padding: 0.5rem 1rem;
+        }
+
+        button {
+            color: white;
+            background-color: $median--primary;
+            border: none;
+            border-radius: 20px;
+            width: 100%;
+            padding: 0.5rem 1rem;
+        }
+    }
+
+    h4,
+    a {
+        color: $cube--light;
+        text-decoration: none;
+        display: block;
+        margin-bottom: 1rem;
+    }
+}
+</style>
