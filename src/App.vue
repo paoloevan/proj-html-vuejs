fff62f31a5e443c83b341d98692e774c9713a892
paoloevan <paoloev1900@gmail.com>
2022-11-21 14:31:11 +0100

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,22 @@
-<script setup>
+<script >
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+
+}
 
 </script>
 
 <template>
-  <header>
-    <div class="container">
-      <div class="logo">
-        <img src="./assets/img/dark-pet-logo.png" alt="">
-      </div>
 
-      <input type="search">
-
-      <div class="text">
-        <strong>Questions? </strong>Call us: 1.800.123.4567
-      </div>
-
-      <div class="personal">
-
-      </div>
-
-    </div>
-  </header>
+  <AppHeader />
   <!-- / header -->
 </template>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@use './assets/sass/partials/palette' as *;
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+
+    <header>
+        <div class="container d-flex justify-content-between align-items-center">
+            <div class="logo">
+                <img src="../assets/img/dark-pet-logo.png" alt="">
+            </div>
+
+            <div class="input_container">
+                <i class="fa-solid fa-magnifying-glass pe-2"></i>
+                <input type="search" placeholder="Search...">
+            </div>
+
+            <div class="text">
+                <strong>Questions? </strong>Call us: 1.800.123.4567
+            </div>
+
+            <div class="personal">
+                <i class="fa-solid fa-check-to-slot me-2"></i>
+                <i class="fa-regular fa-user ms-2"></i>
+            </div>
+
+        </div>
+    </header>
+</template>
+
+
+<style lang="scss" scoped>
+@use '../assets/sass/partials/palette' as *;
+
+header {
+    padding: 1rem;
+    border-bottom: 1px solid $material--light-vibrant;
+
+    .input_container {
+        color: $cube--gray;
+        background-color: $material--light-vibrant;
+        padding: 0.5rem 1rem;
+        border-radius: 20px;
+
+        input {
+            color: $cube--gray;
+            border: none;
+            background-color: $material--light-vibrant;
+
+        }
+    }
+}
+</style>
